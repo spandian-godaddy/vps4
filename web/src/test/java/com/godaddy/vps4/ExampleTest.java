b9b814bb9cdf37f7844f5cb8c48431993b2aa13e
Brian Diekelman <bdiekelman@godaddy.com>
2016-08-30 12:56:19 -0700

    add test cases, configure phase2-tests profile

---
+++ web/src/test/java/com/godaddy/vps4/ExampleTest.java
@@ -0,0 +1,12 @@
+package com.godaddy.vps4;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class ExampleTest {
+
+    @Test
+    public void alwaysPasses() {
+        assertTrue(true);
+    }
+}
---
+++ web/src/test/java/com/godaddy/vps4/phase2/ExampleDatabaseTest.java
@@ -0,0 +1,13 @@
+package com.godaddy.vps4.phase2;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class ExampleDatabaseTest {
+
+    @Test
+    public void alwaysPasses() {
+        assertTrue(true);
+    }
+}
