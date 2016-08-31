c0c9b39c86b8becb8a1f50dd43d88a2b140c2be5
Brian Diekelman <bdiekelman@godaddy.com>
2016-08-30 17:08:35 -0700

    test code coverage

--- web/src/test/java/com/godaddy/vps4/ExampleTest.java
+++ web/src/test/java/com/godaddy/vps4/ExampleTest.java
@@ -1,12 +1,19 @@
 package com.godaddy.vps4;
 
 import org.junit.Test;
+
+import com.godaddy.vps4.security.User;
+
 import static org.junit.Assert.*;
 
 public class ExampleTest {
 
     @Test
     public void alwaysPasses() {
+
+        User user = new User();
+        user.name = "Brian";
+
         assertTrue(true);
     }
 }
