127904585852852b6c350aa75b0bf10b60c1e063
Brian Diekelman <bdiekelman@godaddy.com>
2016-09-01 15:46:13 -0700

    modify phase2 test to verify coverage report

--- web/src/test/java/com/godaddy/vps4/phase2/ExampleDatabaseTest.java
+++ web/src/test/java/com/godaddy/vps4/phase2/ExampleDatabaseTest.java
@@ -1,13 +1,19 @@
 package com.godaddy.vps4.phase2;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
+import static org.junit.Assert.*;
+
+import com.godaddy.vps4.security.User;
+import com.godaddy.vps4.web.UsersResource;
 
 public class ExampleDatabaseTest {
 
     @Test
     public void alwaysPasses() {
+        User user = new User();
+        user.name = "Brian";
+
+        new UsersResource(user).getUser();
         assertTrue(true);
     }
 }
