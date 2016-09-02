c31c88cc901fecc4f9e76d8f3ee0df9606be5a29
Brian Diekelman <bdiekelman@godaddy.com>
2016-09-02 15:06:24 -0700

    pull generic GuiceFilter and Swagger config into their own modules

---
+++ web/src/main/java/com/godaddy/vps4/web/WebModule.java
@@ -0,0 +1,17 @@
+package com.godaddy.vps4.web;
+
+import com.godaddy.vps4.jdbc.DatabaseModule;
+import com.godaddy.vps4.security.UserModule;
+import com.google.inject.AbstractModule;
+
+public class WebModule extends AbstractModule {
+
+    @Override
+    public void configure() {
+
+        install(new DatabaseModule());
+
+        install(new UserModule());
+        bind(UsersResource.class);
+    }
+}
--- web/src/main/java/com/godaddy/vps4/web/WebServer.java
+++ web/src/main/java/com/godaddy/vps4/web/WebServer.java
@@ -15,24 +15,12 @@ import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.util.thread.QueuedThreadPool;
 import org.jboss.resteasy.plugins.guice.GuiceResteasyBootstrapServletContextListener;
-import org.jboss.resteasy.plugins.server.servlet.HttpServletDispatcher;
 import org.jboss.resteasy.util.GetRestful;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JSR310Module;
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
-import com.godaddy.vps4.jdbc.DatabaseModule;
-import com.godaddy.vps4.security.UserModule;
-import com.google.inject.AbstractModule;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.google.inject.Module;
-import com.google.inject.Scopes;
 import com.google.inject.servlet.GuiceFilter;
-import com.google.inject.servlet.ServletModule;
 
 import io.swagger.config.Scanner;
 import io.swagger.config.ScannerFactory;
@@ -65,53 +53,10 @@ public class WebServer {
 
         List<Module> modules = new ArrayList<>();
 
-        modules.add(new ServletModule() {
-            @Override
-            protected void configureServlets() {
+        modules.add(new GuiceFilterModule());
+        modules.add(new SwaggerModule());
 
-                // only inject objects we explicitly bind,
-                // don't attempt runtime introspection/JIT binding
-                binder().requireExplicitBindings();
-
-                // explicitly bind GuiceFilter so we can an instance of
-                // it from the injector later with all our resources injected
-                bind(GuiceFilter.class).in(Scopes.SINGLETON);
-
-                // by binding the servlet context listener here, the 'parentInjector'
-                // field is injected by Guice.  That parentInjector is what contains
-                // all of our application-level bindings
-                bind(GuiceResteasyBootstrapServletContextListener.class);
-
-                // hook Jackson into Jersey as the POJO <-> JSON mapper
-                ObjectMapper mapper = new ObjectMapper();
-                mapper.registerModule(new JSR310Module());
-                mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-
-                JacksonJsonProvider jsonProvider = new JacksonJaxbJsonProvider(mapper, JacksonJaxbJsonProvider.DEFAULT_ANNOTATIONS);
-                jsonProvider.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-
-                bind(JacksonJsonProvider.class).toInstance(jsonProvider);
-
-                // all requests go through RESTEasy dispatcher
-                bind(HttpServletDispatcher.class).in(Scopes.SINGLETON);
-                serve("/*").with(HttpServletDispatcher.class);
-
-                bind(DefaultExceptionMapper.class);
-            }
-        });
-
-        modules.add(new UserModule());
-        modules.add(new DatabaseModule());
-        modules.add(new AbstractModule() {
-            @Override
-            public void configure() {
-                bind(UsersResource.class);
-
-                bind(io.swagger.jaxrs.listing.ApiListingResource.class).in(Scopes.SINGLETON);
-                bind(io.swagger.jaxrs.listing.SwaggerSerializers.class);
-
-            }
-        });
+        modules.add(new WebModule());
 
         Injector injector = Guice.createInjector(modules);
 
