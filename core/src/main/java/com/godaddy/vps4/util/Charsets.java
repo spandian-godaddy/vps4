a3b3bc66fe7f577aa62656ba5d6fe7a9572d813b
Brian Diekelman <bdiekelman@godaddy.com>
2016-09-09 16:01:11 -0700

    make Charsets an interface

--- core/src/main/java/com/godaddy/vps4/util/Charsets.java
+++ core/src/main/java/com/godaddy/vps4/util/Charsets.java
@@ -2,11 +2,8 @@ package com.godaddy.vps4.util;
 
 import java.nio.charset.Charset;
 
-public class Charsets {
+public interface Charsets {
 
     public static final Charset UTF8 = Charset.forName("UTF-8");
 
-    protected Charsets() {
-        // defeat instantiation
-    }
 }
