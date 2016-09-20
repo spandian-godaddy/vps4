d12b90dcc43216b0bd82d97b67497fe08579d6a7
David Orr <dorr@godaddy.com>
2016-09-20 11:42:03 -0700

    Fix reference for VmService

--- core/src/main/java/com/godaddy/vps4/vm/VmModule.java
+++ core/src/main/java/com/godaddy/vps4/vm/VmModule.java
@@ -3,14 +3,13 @@ package com.godaddy.vps4.vm;
 import javax.inject.Singleton;
 
 import com.godaddy.vps4.hfs.HfsClientProvider;
+import com.godaddy.vps4.hfs.VmService;
 import com.godaddy.vps4.security.PrivilegeService;
 import com.godaddy.vps4.security.jdbc.JdbcPrivilegeService;
 import com.godaddy.vps4.vm.jdbc.JdbcControlPanelService;
 import com.godaddy.vps4.vm.jdbc.JdbcOsTypeService;
 import com.godaddy.vps4.vm.jdbc.JdbcVirtualMachineService;
 import com.google.inject.AbstractModule;
-
-import gdg.hfs.vhfs.vm.VmService;
 
 public class VmModule extends AbstractModule {
 
@@ -22,5 +21,4 @@ public class VmModule extends AbstractModule {
 		bind(OsTypeService.class).to(JdbcOsTypeService.class);
 		bind(VmService.class).toProvider(new HfsClientProvider<VmService>(VmService.class)).in(Singleton.class);
 	}
-
 }
