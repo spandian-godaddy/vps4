cbeee5d7f54624f3e578269cecfce7b4e1c424be
Brian Diekelman <bdiekelman@godaddy.com>
2016-09-26 12:08:31 -0700

    remove git diff text from ProvisionVmWorker

--- web/src/main/java/com/godaddy/vps4/web/vm/ProvisionVmWorker.java
+++ web/src/main/java/com/godaddy/vps4/web/vm/ProvisionVmWorker.java
@@ -7,29 +7,25 @@ import com.godaddy.vps4.hfs.Flavor;
 import com.godaddy.vps4.hfs.VmAction;
 import com.godaddy.vps4.hfs.VmService;
 import com.godaddy.vps4.hfs.VmService.FlavorList;
-<<<<<<< Updated upstream
+
 import com.godaddy.vps4.web.vm.VmsResource.ActionStatus;
 import com.godaddy.vps4.web.vm.VmsResource.CreateVmAction;
-=======
-import com.godaddy.vps4.web.Action.ActionStatus;
-import com.godaddy.vps4.web.vm.VmResource.CreateVmAction;
->>>>>>> Stashed changes
 
 public class ProvisionVmWorker implements Runnable {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(ProvisionVmWorker.class);
 
 	final VmService vmService;
-	
+
 	final CreateVmAction action;
-	
+
 	public ProvisionVmWorker(VmService vmService, CreateVmAction action) {
 		this.vmService = vmService;
 		this.action = action;
 	}
-	
+
 	protected Flavor findFlavor(String name) {
-		
+
 		FlavorList flavors = vmService.getFlavors();
 		if (flavors != null && flavors.results != null) {
 			for (Flavor flavor : flavors.results) {
@@ -40,40 +36,40 @@ public class ProvisionVmWorker implements Runnable {
 		}
 		return null;
 	}
-	
+
 	@Override
 	public void run() {
-		
+
         logger.info("sending HFS VM request: {}", action.hfsProvisionRequest);
-		
+
         VmAction hfsAction = vmService.createVm(action.hfsProvisionRequest);
-		
+
 		// wait for VmAction to complete
 		while (!hfsAction.state.equals("COMPLETE")) {
-			
+
 			logger.info("waiting on VM to provision: {}", hfsAction);
-			
+
 			if (hfsAction.state.equals("IN_PROGRESS")) {
 				action.vm = vmService.getVm(hfsAction.vmId);
 				synchronized (this) {
 				    this.notify();
 				}
 			}
-			
+
 			// give the VM time to spin up
 			try {
 				Thread.sleep(2000);
 			} catch (InterruptedException e) {
 				logger.warn("Interrupted while sleeping");
 			}
-			
+
 			hfsAction = vmService.getVmAction(hfsAction.vmId, hfsAction.vmActionId);
 		}
-		
+
         logger.info("provisioning complete: {}", hfsAction);
 
-		action.vm = vmService.getVm(hfsAction.vmId);		
+		action.vm = vmService.getVm(hfsAction.vmId);
 		action.status = ActionStatus.COMPLETE;
 	}
-	
+
 }
