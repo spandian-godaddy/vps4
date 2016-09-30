929a92329c492462419a152150da688816e81afb
David Orr <dorr@godaddy.com>
2016-09-30 10:39:38 -0700

    Add unit test for hostname generator

---
+++ core/src/test/java/com/godaddy/vps4/HostnameGeneratorTest.java
@@ -0,0 +1,20 @@
+package com.godaddy.vps4;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+import com.godaddy.vps4.vm.HostnameGenerator;
+
+public class HostnameGeneratorTest {
+
+    @Test
+    public void testGetHostname() {
+        String ipAddress = "192.168.1.1";
+
+        String hostname = HostnameGenerator.getHostname(ipAddress);
+
+        assertEquals("s192-168-1-1.secureserver.net", hostname);
+    }
+
+}
