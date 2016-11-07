42d3178679ad876ab6ba08144da7e251c4d3aa6e
David Orr <dorr@godaddy.com>
2016-11-07 14:49:20 -0700

    Remove the VM interface and other objects

--- web/src/main/java/com/godaddy/vps4/web/hfs/HfsModule.java
+++ web/src/main/java/com/godaddy/vps4/web/hfs/HfsModule.java
@@ -3,19 +3,19 @@ package com.godaddy.vps4.web.hfs;
 import javax.inject.Singleton;
 
 import com.godaddy.vps4.hfs.HfsClientProvider;
-import com.godaddy.vps4.hfs.VmService;
 import com.google.inject.AbstractModule;
 
 import gdg.hfs.vhfs.cpanel.CPanelService;
 import gdg.hfs.vhfs.network.NetworkService;
 import gdg.hfs.vhfs.sysadmin.SysAdminService;
+import gdg.hfs.vhfs.vm.VmService;
 
 public class HfsModule extends AbstractModule {
 
     @Override
     public void configure() {
         bind(SysAdminService.class).toProvider(new HfsClientProvider<SysAdminService>(SysAdminService.class)).in(Singleton.class);
-        bind(VmService.class).toProvider(new HfsClientProvider<VmService>(VmService.class)).in(Singleton.class);
+        bind(gdg.hfs.vhfs.vm.VmService.class).toProvider(new HfsClientProvider<VmService>(VmService.class)).in(Singleton.class);
         bind(NetworkService.class).toProvider(new HfsClientProvider<NetworkService>(NetworkService.class)).in(Singleton.class);
         bind(CPanelService.class).toProvider(new HfsClientProvider<CPanelService>(CPanelService.class)).in(Singleton.class);
     }
