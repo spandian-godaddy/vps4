35d0897834ba660580ca3a2f9ca514eafb1522ee
David Orr <dorr@godaddy.com>
2016-11-07 15:38:15 -0700

    Cleanup

--- web/src/main/java/com/godaddy/vps4/web/hfs/HfsModule.java
+++ web/src/main/java/com/godaddy/vps4/web/hfs/HfsModule.java
@@ -15,7 +15,7 @@ public class HfsModule extends AbstractModule {
     @Override
     public void configure() {
         bind(SysAdminService.class).toProvider(new HfsClientProvider<SysAdminService>(SysAdminService.class)).in(Singleton.class);
-        bind(gdg.hfs.vhfs.vm.VmService.class).toProvider(new HfsClientProvider<VmService>(VmService.class)).in(Singleton.class);
+        bind(VmService.class).toProvider(new HfsClientProvider<VmService>(VmService.class)).in(Singleton.class);
         bind(NetworkService.class).toProvider(new HfsClientProvider<NetworkService>(NetworkService.class)).in(Singleton.class);
         bind(CPanelService.class).toProvider(new HfsClientProvider<CPanelService>(CPanelService.class)).in(Singleton.class);
     }
