c6824daaa2cdc89a6eb4f3822978ac7c92485c12
Atul S. Bhoite <abhoite@godaddy.com>
2016-11-22 16:00:19 -0700

    STATE field is JSON.

--- core/sql/migrations/005_vm_action/3_vm_action.sql
+++ core/sql/migrations/005_vm_action/3_vm_action.sql
@@ -4,7 +4,7 @@ CREATE TABLE vm_action (
 	, action_type_id		BIGINT		NOT NULL	REFERENCES action_type(type_id) DEFAULT 1
 	, vps4_user_id			BIGINT		NOT NULL	REFERENCES vps4_user(vps4_user_id)
 	, request				JSON
-	, state                 VARCHAR(255)
+	, state                 JSON
 	, response				JSON
 	, status_id				BIGINT		NOT NULL  	REFERENCES action_status(status_id) DEFAULT 1
 	, created				TIMESTAMP	NOT NULL	DEFAULT NOW()
