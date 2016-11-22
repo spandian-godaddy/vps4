3d9d0dadf3766f06c601b1484d627471a6875d11
Brian Diekelman <bdiekelman@godaddy.com>
2016-11-22 14:37:46 -0700

    add test VM Request auto-creation tool

---
+++ web/src/test/java/com/godaddy/vps4/web/security/AutoCreateVps4UserModule.java
@@ -0,0 +1,27 @@
+package com.godaddy.vps4.web.security;
+
+import com.godaddy.vps4.security.Vps4User;
+import com.godaddy.vps4.security.Vps4UserService;
+import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
+
+public class AutoCreateVps4UserModule extends AbstractModule {
+
+    final String shopperId;
+
+    public AutoCreateVps4UserModule(String shopperId) {
+        this.shopperId = shopperId;
+    }
+
+    @Override
+    protected void configure() {
+
+    }
+
+    @Provides
+    protected Vps4User provideUser(Vps4UserService userService) {
+
+        return userService.getOrCreateUserForShopper(shopperId);
+    }
+
+}
---
+++ web/src/test/java/com/godaddy/vps4/web/vm/CreateTestVmRequest.java
@@ -0,0 +1,75 @@
+package com.godaddy.vps4.web.vm;
+
+import java.util.Random;
+import java.util.UUID;
+
+import javax.inject.Inject;
+
+import com.godaddy.vps4.config.ConfigModule;
+import com.godaddy.vps4.jdbc.DatabaseModule;
+import com.godaddy.vps4.project.ProjectService;
+import com.godaddy.vps4.security.SecurityModule;
+import com.godaddy.vps4.security.Vps4User;
+import com.godaddy.vps4.security.Vps4UserService;
+import com.godaddy.vps4.vm.ImageService;
+import com.godaddy.vps4.vm.VirtualMachineService;
+import com.godaddy.vps4.vm.VirtualMachineSpec;
+import com.godaddy.vps4.vm.VmModule;
+import com.godaddy.vps4.web.security.AutoCreateVps4UserModule;
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+public class CreateTestVmRequest {
+
+    final VirtualMachineService virtualMachineService;
+
+    final ProjectService projectService;
+
+    final ImageService imageService;
+
+    final Vps4UserService userService;
+
+    @Inject
+    public CreateTestVmRequest(
+            VirtualMachineService virtualMachineService,
+            ProjectService projectService,
+            ImageService imageService,
+            Vps4UserService userService) {
+        this.virtualMachineService = virtualMachineService;
+        this.projectService = projectService;
+        this.imageService = imageService;
+        this.userService = userService;
+    }
+
+    static final String SHOPPER_ID = "5y5";
+
+    protected void provision(int tier, int managedLevel, String operatingSystem, String controlPanel) {
+
+        VirtualMachineSpec spec = virtualMachineService.getSpec(tier);
+
+        UUID orionGuid = UUID.randomUUID();
+        virtualMachineService.createVirtualMachineRequest(orionGuid, operatingSystem, controlPanel, tier, managedLevel);
+
+        // normally we would get this from HFS
+        long vmId = new Random().nextInt(1000000);
+
+        Vps4User user = userService.getOrCreateUserForShopper(SHOPPER_ID);
+
+        long projectId = projectService.createProject("My Cool Project", user.getId(), 1).getProjectId();
+
+        int imageId = imageService.getImageId(operatingSystem);
+
+        virtualMachineService.provisionVirtualMachine(vmId, orionGuid, "SomeNewVm", projectId, spec.specId, managedLevel, imageId);
+    }
+
+    public static void main(String[] args) {
+        Injector injector = Guice.createInjector(
+                new ConfigModule(), new DatabaseModule(),
+                new VmModule(), new SecurityModule(), new AutoCreateVps4UserModule(SHOPPER_ID));
+
+        CreateTestVmRequest force = injector.getInstance(CreateTestVmRequest.class);
+
+        force.provision(10, 0, "centos-7", "cpanel");
+    }
+
+}
