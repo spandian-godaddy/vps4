6941c168f802a03ae2a0c656b54494e8cba8ea1b
Brian Diekelman <bdiekelman@godaddy.com>
2016-11-22 19:18:17 -0700

    undo bad merge of old Config methods (Config shouldn't know about types)

--- core/src/main/java/com/godaddy/vps4/config/Config.java
+++ core/src/main/java/com/godaddy/vps4/config/Config.java
@@ -1,12 +1,8 @@
 package com.godaddy.vps4.config;
 
-import java.time.Duration;
-
 public interface Config {
 
     String get(String key, String defaultValue);
 
     String get(String key);
-
-    Duration getDuration(String configProperty, Duration defaultPropertyValue);
 }
