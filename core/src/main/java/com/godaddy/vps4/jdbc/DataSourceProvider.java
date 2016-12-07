88721b2ae3bff05980c04a52d520e4501ff36ae1
Brian Diekelman <bdiekelman@godaddy.com>
2016-12-06 17:28:39 -0700

    DataSources - global data source registry

--- core/src/main/java/com/godaddy/vps4/jdbc/DataSourceProvider.java
+++ core/src/main/java/com/godaddy/vps4/jdbc/DataSourceProvider.java
@@ -4,16 +4,9 @@ import javax.inject.Inject;
 import javax.inject.Provider;
 import javax.sql.DataSource;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import com.godaddy.vps4.config.Config;
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
 
 public class DataSourceProvider implements Provider<DataSource> {
-
-    private static final Logger logger = LoggerFactory.getLogger(DataSourceProvider.class);
 
     private final Config config;
 
@@ -41,30 +34,16 @@ public class DataSourceProvider implements Provider<DataSource> {
     protected DataSource buildDataSource() {
         // build datasource from config
         String server = config.get("db.vps4.server");
-        String username = config.get("db.vps4.username");
-        String password = config.get("db.vps4.password");
         String database = config.get("db.vps4.database");
-
         int port = Integer.parseInt(config.get("db.vps4.port"));
 
-        String jdbcUrl = "jdbc:postgresql://" + server + ":" + port + "/" + database;;
+        String jdbcUrl = "jdbc:postgresql://" + server + ":" + port + "/" + database;
 
-        logger.info("building datasource to {}@{}", new Object[]{username, jdbcUrl});
+        String username = config.get("db.vps4.username");
+        String password = config.get("db.vps4.password");
 
-        HikariConfig dsConfig = new HikariConfig();
-        dsConfig.setJdbcUrl(jdbcUrl);
-        dsConfig.setUsername(username);
-        dsConfig.setPassword(password);
-
-        final HikariDataSource dataSource = new HikariDataSource(dsConfig);;
-
-        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
-            @Override
-            public void run() {
-                logger.info("shutting down vertical database connection pool");
-                dataSource.close();
-            }
-        }));
-        return dataSource;
+        return DataSources.getDataSource(jdbcUrl, username, password);
     }
+
+
 }
---
+++ core/src/main/java/com/godaddy/vps4/jdbc/DataSources.java
@@ -0,0 +1,65 @@
+package com.godaddy.vps4.jdbc;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
+public class DataSources {
+
+    private static final Logger logger = LoggerFactory.getLogger(DataSources.class);
+
+    private static final Map<String, HikariDataSource> dataSources = new ConcurrentHashMap<>();
+
+    private DataSources() {
+        // defeat instantiation
+    }
+
+    public static DataSource getDataSource(String jdbcUrl, String username, String password) {
+
+        String key = username + "@" + jdbcUrl;
+
+        HikariDataSource dataSource = dataSources.computeIfAbsent(key, k -> {
+
+            logger.info("building datasource to {}", key);
+
+            HikariConfig dsConfig = new HikariConfig();
+            dsConfig.setJdbcUrl(jdbcUrl);
+            dsConfig.setUsername(username);
+            dsConfig.setPassword(password);
+
+            return new HikariDataSource(dsConfig);
+        });
+
+        return dataSource;
+    }
+
+    static {
+        Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+                logger.info("closing all database connection pools");
+
+                for (Map.Entry<String, HikariDataSource> dsEntry : dataSources.entrySet()) {
+
+                    String url = dsEntry.getKey();
+                    HikariDataSource dataSource = dsEntry.getValue();
+
+                    logger.info("closing database connection pool: {}", url);
+                    try {
+                        dataSource.close();
+                    } catch (Exception e) {
+                        logger.error("Error closing database connection pool", e);
+                    }
+                }
+            }
+        }));
+    }
+}
