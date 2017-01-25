42637007e1b0924429a7bc1653a4bf2c3515ea27
David Orr <dorr@godaddy.com>
2017-01-25 15:09:06 -0700

    Remove credit update in provision function

--- core/sql/migrations/V0.14.0__reserve_credit_on_provision.sql
+++ core/sql/migrations/V0.14.0__reserve_credit_on_provision.sql
@@ -16,3 +16,21 @@ BEGIN
 END 
 $$ LANGUAGE plpgsql;
     
+
+CREATE OR REPLACE FUNCTION virtual_machine_provision(p_vm_id UUID
+    , p_orion_guid UUID 
+    , p_name TEXT
+    , p_project_id BIGINT
+    , p_spec_id INT
+    , p_managed_level INT
+    , p_image_id BIGINT)
+
+    RETURNS VOID AS $$
+DECLARE
+BEGIN
+    
+    INSERT INTO virtual_machine (vm_id, orion_guid, name, project_id, spec_id, managed_level, image_id)
+       VALUES (p_vm_id, p_orion_guid, p_name, p_project_id, p_spec_id, p_managed_level, p_image_id);
+       
+END;
+$$ LANGUAGE plpgsql;
