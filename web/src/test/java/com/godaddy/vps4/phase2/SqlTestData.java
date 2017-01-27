291b3eaae98e5b1733fc7df72ecbe082983d3232
David Orr <dorr@godaddy.com>
2017-01-26 17:43:58 -0700

    Fix tests

---
+++ web/src/test/java/com/godaddy/vps4/phase2/SqlTestData.java
@@ -0,0 +1,48 @@
+package com.godaddy.vps4.phase2;
+
+import java.util.UUID;
+
+import javax.sql.DataSource;
+
+import com.godaddy.vps4.jdbc.Sql;
+import com.godaddy.vps4.vm.VirtualMachine;
+import com.godaddy.vps4.vm.VirtualMachineService;
+import com.godaddy.vps4.vm.VirtualMachineService.ProvisionVirtualMachineParameters;
+import com.godaddy.vps4.vm.jdbc.JdbcVirtualMachineService;
+
+
+public class SqlTestData {
+
+    public static long getNextHfsVmId(DataSource dataSource) {
+        return Sql.with(dataSource).exec("SELECT max(hfs_vm_id) as hfs_vm_id FROM virtual_machine",
+                Sql.nextOrNull(rs -> rs.isAfterLast() ? 0 : rs.getLong("hfs_vm_id"))) + 1;
+    }
+
+    public static VirtualMachine insertTestVm(UUID orionGuid, DataSource dataSource) {
+        return insertTestVm(orionGuid, 1, dataSource);
+    }
+
+    public static VirtualMachine insertTestVm(UUID orionGuid, long vps4UserId, DataSource dataSource) {
+        VirtualMachineService virtualMachineService = new JdbcVirtualMachineService(dataSource);
+        long hfsVmId = getNextHfsVmId(dataSource);
+        virtualMachineService.createVirtualMachineCredit(orionGuid, "linux", "none", 10, 0, "TestUser");
+        ProvisionVirtualMachineParameters params = new ProvisionVirtualMachineParameters(vps4UserId, 1, "vps4-testing-", orionGuid,
+                "testVirtualMachine", 10, 1, "centos-7");
+        VirtualMachine virtualMachine = virtualMachineService.provisionVirtualMachine(params);
+        virtualMachineService.addHfsVmIdToVirtualMachine(virtualMachine.vmId, hfsVmId);
+        return virtualMachine;
+    }
+
+    public static void cleanupTestVmAndRelatedData(UUID vmId, DataSource dataSource) {
+        VirtualMachineService virtualMachineService = new JdbcVirtualMachineService(dataSource);
+        VirtualMachine vm = virtualMachineService.getVirtualMachine(vmId);
+
+        Sql.with(dataSource).exec("DELETE FROM ip_address WHERE vm_id = ?", null, vm.vmId);
+        Sql.with(dataSource).exec("DELETE FROM vm_user WHERE vm_id = ?", null, vm.vmId);
+        Sql.with(dataSource).exec("DELETE FROM vm_action where vm_id = ?", null, vm.vmId);
+        Sql.with(dataSource).exec("DELETE FROM virtual_machine WHERE vm_id = ?", null, vmId);
+        Sql.with(dataSource).exec("DELETE FROM credit WHERE orion_guid = ?", null, vm.orionGuid);
+        Sql.with(dataSource).exec("DELETE FROM user_project_privilege WHERE project_id = ?", null, vm.projectId);
+        Sql.with(dataSource).exec("DELETE FROM project WHERE project_id = ?", null, vm.projectId);
+    }
+}
