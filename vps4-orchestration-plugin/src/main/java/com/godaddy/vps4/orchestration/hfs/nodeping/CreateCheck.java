98eecaf0848af1a31ab184a443a9a4b3388c9266
David Orr <dorr@godaddy.com>
2017-03-30 15:57:04 -0700

    Add details of createcheck and deletecheck

--- vps4-orchestration-plugin/src/main/java/com/godaddy/vps4/orchestration/hfs/nodeping/CreateCheck.java
+++ vps4-orchestration-plugin/src/main/java/com/godaddy/vps4/orchestration/hfs/nodeping/CreateCheck.java
@@ -1,5 +1,45 @@
 package com.godaddy.vps4.orchestration.hfs.nodeping;
 
-public class CreateCheck {
+import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Inject;
+
+import gdg.hfs.orchestration.Command;
+import gdg.hfs.orchestration.CommandContext;
+import gdg.hfs.vhfs.nodeping.NodePingAction;
+import gdg.hfs.vhfs.nodeping.NodePingService;
+
+public class CreateCheck implements Command<CreateCheck.Request, NodePingAction> {
+    
+    private static final Logger logger = LoggerFactory.getLogger(CreateCheck.class);
+    
+    public static class Request {
+        public long accountId;
+        public String target;
+        public String label;
+    }
+    
+    final NodePingService nodePingService;
+    
+    @Inject
+    public CreateCheck(NodePingService nodePingService) {
+        this.nodePingService = nodePingService;
+    }
+    
+    @Override
+    public NodePingAction execute(CommandContext context, CreateCheck.Request request) {
+        
+        logger.info("Sending HFS request to create nodeping check for target: {}", request.target);
+        
+        NodePingAction hfsAction = context.execute("RequestCreateNodepingCheck",
+                ctx -> nodePingService.createCheck(request.accountId, request.target, request.label));
+        
+        hfsAction = context.execute(WaitForNodePingAction.class, hfsAction);
+        
+        Response checks = nodePingService.getChecks(request.accountId);
+        return hfsAction;
+    }
 }
--- vps4-orchestration-plugin/src/main/java/com/godaddy/vps4/orchestration/hfs/nodeping/DeleteCheck.java
+++ vps4-orchestration-plugin/src/main/java/com/godaddy/vps4/orchestration/hfs/nodeping/DeleteCheck.java
@@ -1,5 +1,44 @@
 package com.godaddy.vps4.orchestration.hfs.nodeping;
 
-public class DeleteCheck {
+    import javax.ws.rs.core.Response;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.google.inject.Inject;
+
+import gdg.hfs.orchestration.Command;
+import gdg.hfs.orchestration.CommandContext;
+import gdg.hfs.vhfs.nodeping.NodePingAction;
+import gdg.hfs.vhfs.nodeping.NodePingService;
+
+public class DeleteCheck implements Command<DeleteCheck.Request, NodePingAction>{
+
+    private static final Logger logger = LoggerFactory.getLogger(DeleteCheck.class);
+
+    public static class Request {
+        public long accountId;
+        public String checkId;
+    }
+
+    final NodePingService nodePingService;
+
+    @Inject
+    public DeleteCheck(NodePingService nodePingService) {
+        this.nodePingService = nodePingService;
+    }
+
+    @Override
+    public NodePingAction execute(CommandContext context, DeleteCheck.Request request) {
+        
+        logger.info("Sending HFS request to delete nodeping check: {}", request.checkId);
+        
+        NodePingAction hfsAction = context.execute("RequestDeleteNodepingCheck",
+                ctx -> nodePingService.deleteCheck(request.accountId, request.checkId));
+        
+        hfsAction = context.execute(WaitForNodePingAction.class, hfsAction);
+        
+        Response checks = nodePingService.getChecks(request.accountId);
+        return hfsAction;
+    }
 }
