cea704899d6592b6d60b8771e5c903ce41e4bf25
Scott Johnson <sjohnson@godaddy.com>
2017-04-20 14:35:32 -0700

    Add warning note on enabling Ecomm Credit Service

--- core/src/main/java/com/godaddy/vps4/credit/CreditModule.java
+++ core/src/main/java/com/godaddy/vps4/credit/CreditModule.java
@@ -4,13 +4,13 @@ import com.godaddy.vps4.credit.jdbc.JdbcCreditService;
 import com.google.inject.AbstractModule;
 import com.google.inject.Singleton;
 
-import gdg.hfs.vhfs.ecomm.ECommService;
-
 public class CreditModule extends AbstractModule{
 
     @Override
     public void configure() {
+
         bind(CreditService.class).to(JdbcCreditService.class).in(Singleton.class);
-//        bind(CreditService.class).to(ECommCreditService.class).in(Singleton.class);
+        // TODO: Disable createCreditIfNoneExists() in authenticate before swapping binding - Jira VPS4-469
+        // bind(CreditService.class).to(ECommCreditService.class).in(Singleton.class);
     }
 }
