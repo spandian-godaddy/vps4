d709d3b83f9d922a56672a7e759bd977d79336b5
Akash Shetty <ashetty@godaddy.com>
2018-02-02 13:42:27 -0700

    data migration for windows 2016/plesk support (#416)

---
+++ core/sql/migrations/V0.29.0__add_windows_images.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.image(
+    image_id, hfs_name, control_panel_id, os_type_id, name)
+    VALUES (10, 'hfs-windows-2016', 0, 2, 'Windows 2016');
+INSERT INTO public.image(
+    image_id, hfs_name, control_panel_id, os_type_id, name)
+    VALUES (11, 'hfs-windows-2016-plesk-17', 2, 2, 'Windows 2016 (Plesk 12.5)');
---
+++ core/sql/migrations/V0.29.1__deprecate_old_windows_images.sql
@@ -0,0 +1,1 @@
+UPDATE image SET valid_until = valid_on WHERE image_id in (6, 8);
