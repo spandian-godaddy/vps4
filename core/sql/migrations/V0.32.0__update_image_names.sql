e652a2b42d1e273abcb377911aca5118238cc986
Scott Mangiapane <smangiapane@godaddy.com>
2018-06-12 14:32:06 -0700

    Removed version numbers from image names (#518)

---
+++ core/sql/migrations/V0.32.0__update_image_names.sql
@@ -0,0 +1,3 @@
+UPDATE image SET name='CentOS 7 (cPanel)' WHERE name='CentOS 7 (cPanel 11)';
+UPDATE image SET name='Windows 2012 R2 (Plesk)' WHERE name='Windows 2012 R2 (Plesk 17.0.17)';
+UPDATE image SET name='Windows 2016 (Plesk)' WHERE name='Windows 2016 (Plesk 12.5)';
