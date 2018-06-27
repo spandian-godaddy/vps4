8804b69fab95bd6c9dd061b14e6ac80283861922
gsheppard <gsheppard@godaddy.com>
2018-06-27 12:34:13 -0700

    reset sequence start point (#536)

--- core/sql/migrations/V0.33.3__add_ubuntu_plesk_image.sql
+++ core/sql/migrations/V0.33.3__add_ubuntu_plesk_image.sql
@@ -1,3 +1,6 @@
+-- V0.29.0__add_windows_images.sql updated the ids directly, and did not increment the sequence
+--  This will reset the sequence to where it should be
+ALTER SEQUENCE image_image_id_seq RESTART WITH 12;
 INSERT INTO image
   (hfs_name, control_panel_id, os_type_id, name)
 VALUES
