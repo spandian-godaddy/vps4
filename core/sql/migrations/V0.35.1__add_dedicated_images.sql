84eb58cffc9301b5b032164a1e95f080dfb42faa
gsheppard <gsheppard@godaddy.com>
2018-10-12 14:35:48 -0700

    add new ded4 images to database (#605)
    * add new ded4 images to database
    
    * Remove other ovh zone entries until we get new values

---
+++ core/sql/migrations/V0.35.1__add_dedicated_images.sql
@@ -0,0 +1,9 @@
+INSERT INTO image
+  (hfs_name, control_panel_id, os_type_id, name, server_type_id)
+VALUES
+  ('centos7_64', 0, 1, 'CentOS 7', 2),
+  ('debian9_64', 0, 1, 'Debian 9', 2),
+  ('ubuntu1604-server_64', 0, 1, 'Ubuntu 16.04', 2),
+  ('ubuntu1804-server_64', 0, 1, 'Ubuntu 18.04', 2),
+  ('win2016-dc_64', 0, 2, 'Windows 2016 Datacenter', 2),
+  ('win2016-std_64', 0, 2, 'Windows 2016 Standard', 2);
