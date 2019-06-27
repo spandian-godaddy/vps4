cbf5f03fdf19db70b2e7ea437a26137c8db0123f
David <dorr@godaddy.com>
2019-06-27 14:46:21 -0700

    Update adv 4 specs memory from gib to mib (#764)

---
+++ core/sql/migrations/V0.46.2__fix_dedv_adv4_mem.sql
@@ -0,0 +1,4 @@
+UPDATE virtual_machine_spec SET memory_mib = 131072 WHERE tier = 100;
+UPDATE virtual_machine_spec SET memory_mib = 262144 WHERE tier = 110;
+UPDATE virtual_machine_spec SET memory_mib = 131072 WHERE tier = 160;
+UPDATE virtual_machine_spec SET memory_mib = 262144 WHERE tier = 170;
