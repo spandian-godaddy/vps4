8fd94d85404d2f2d2410c2bf28ea8b0f77bc9064
David <dorr@godaddy.com>
2019-06-27 12:45:38 -0700

    Enable specs for ded4 adv4 (#763)
    * Update machine specs for ded adv4
    
    * enable the ded4 adv4 specs

---
+++ core/sql/migrations/V0.46.1__enable_ded4_adv4_specs.sql
@@ -0,0 +1,4 @@
+UPDATE virtual_machine_spec SET valid_until = 'infinity' WHERE tier = 100;
+UPDATE virtual_machine_spec SET valid_until = 'infinity' WHERE tier = 110;
+UPDATE virtual_machine_spec SET valid_until = 'infinity' WHERE tier = 160;
+UPDATE virtual_machine_spec SET valid_until = 'infinity' WHERE tier = 170;
