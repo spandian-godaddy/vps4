24b75787d8d5906f91a5c22d4fb0b8c37687d014
Diep Tran (Intern) <dtran1@godaddy.com>
2019-07-08 16:04:10 -0700

    updated service client to return vmaction (#770)

--- web/src/main/java/com/godaddy/vps4/web/client/VmShopperMergeService.java
+++ web/src/main/java/com/godaddy/vps4/web/client/VmShopperMergeService.java
@@ -10,11 +10,13 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import static com.godaddy.vps4.web.vm.VmShopperMergeResource.ShopperMergeRequest;
 
+import com.godaddy.vps4.vm.VmAction;
+
 @Path("/api/vms")
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public interface VmShopperMergeService {
     @POST
     @Path("{vmId}/mergeShopper")
-    void mergeShopper(@PathParam("vmId") UUID vmId, ShopperMergeRequest shopperMergeRequest);
+    VmAction mergeShopper(@PathParam("vmId") UUID vmId, ShopperMergeRequest shopperMergeRequest);
 }
