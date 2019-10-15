18d9bc3e6d20b422ec591bcec851f67e29f4a4ff
Scott Mangiapane <smangiapane@godaddy.com>
2019-10-15 13:52:00 -0700

    Panopta usage endpoint (#823)
    Expanded usage endpoint to support Panopta

--- web/src/main/java/com/godaddy/vps4/web/vm/ServerUsageStatsResource.java
+++ web/src/main/java/com/godaddy/vps4/web/vm/ServerUsageStatsResource.java
@@ -1,5 +1,7 @@
 package com.godaddy.vps4.web.vm;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.UUID;
 
 import javax.inject.Inject;
@@ -12,6 +14,12 @@ import javax.ws.rs.core.MediaType;
 
 import com.godaddy.hfs.vm.ServerUsageStats;
 import com.godaddy.hfs.vm.VmService;
+import com.godaddy.vps4.panopta.PanoptaDataService;
+import com.godaddy.vps4.panopta.PanoptaDetail;
+import com.godaddy.vps4.panopta.PanoptaGraph;
+import com.godaddy.vps4.panopta.PanoptaService;
+import com.godaddy.vps4.panopta.PanoptaServiceException;
+import com.godaddy.vps4.vm.ServerSpec;
 import com.godaddy.vps4.vm.ServerUsageStatsService;
 import com.godaddy.vps4.vm.VirtualMachine;
 import com.godaddy.vps4.web.Vps4Api;
@@ -32,27 +40,32 @@ public class ServerUsageStatsResource {
     private final VmResource vmResource;
     private final ServerUsageStatsService serverUsageStatsService;
     private final VmService vmService;
+    private final PanoptaService panoptaService;
+    private final PanoptaDataService panoptaDataService;
 
     @Inject
-    public ServerUsageStatsResource(VmResource vmResource, ServerUsageStatsService serverUsageStatsService, VmService vmService) {
+    public ServerUsageStatsResource(VmResource vmResource,
+                                    ServerUsageStatsService serverUsageStatsService,
+                                    VmService vmService,
+                                    PanoptaService panoptaService,
+                                    PanoptaDataService panoptaDataService) {
         this.vmResource = vmResource;
         this.serverUsageStatsService = serverUsageStatsService;
         this.vmService = vmService;
+        this.panoptaService = panoptaService;
+        this.panoptaDataService = panoptaDataService;
     }
 
     @GET
     @Path("{vmId}/usage")
-    @ApiOperation(value = "Get server usage stats",
+    @ApiOperation(value = "Get server usage stats from Panopta or HFS",
             notes = "Get the usage stats for the specified server.")
-    public UsageStats getUsage(@PathParam("vmId") UUID vmId) {
+    public UsageStats getUsage(@PathParam("vmId") UUID vmId) throws PanoptaServiceException {
         VirtualMachine vm = vmResource.getVm(vmId);
         verifyServerIsActive(vm.hfsVmId);
 
-        ServerUsageStats serverUsageStats = serverUsageStatsService.getServerUsage(vm.hfsVmId);
-        if (serverUsageStats == null) {
-            throw new Vps4Exception("USAGE_STATS_UNAVAILABLE", "Usage stats are unavailable at the moment.");
-        }
-        return mapToUsageStats(serverUsageStats);
+        PanoptaDetail detail = panoptaDataService.getPanoptaDetails(vmId);
+        return (detail != null) ? getPanoptaUsage(vmId, vm.spec) : getHfsUsage(vm.hfsVmId);
     }
 
     void verifyServerIsActive(long hfsVmId) {
@@ -63,7 +76,75 @@ public class ServerUsageStatsResource {
         }
     }
 
-    private UsageStats mapToUsageStats(ServerUsageStats serverUsageStats) {
+    private UsageStats getPanoptaUsage(UUID vmId, ServerSpec spec) throws PanoptaServiceException {
+        List<PanoptaGraph> graphs = panoptaService.getUsageGraphs(vmId, "hour");
+        if (graphs == null) {
+            throw new Vps4Exception("USAGE_STATS_UNAVAILABLE", "Usage stats are unavailable at the moment.");
+        }
+        return mapPanoptaUsageStats(spec, graphs);
+    }
+
+    private UsageStats mapPanoptaUsageStats(ServerSpec spec, List<PanoptaGraph> graphs) {
+        UsageStats stats = new UsageStats();
+        stats.lastRefreshedAt = Instant.now();
+        for (PanoptaGraph graph : graphs) {
+            try {
+                switch (graph.type) {
+                    case CPU:
+                        UsageStats.CpuUsage newCpu = new UsageStats.CpuUsage();
+                        newCpu.cpuUsagePercent = getLastDouble(graph.values);
+                        stats.cpu = newCpu;
+                        break;
+                    case RAM:
+                        int memMib = spec.memoryMib;
+                        UsageStats.MemUsage newMem = new UsageStats.MemUsage();
+                        newMem.memUsed = (long) (memMib * getLastDouble(graph.values) / 100); // calculate percentage
+                        newMem.memTotal = spec.memoryMib;
+                        stats.mem = newMem;
+                        break;
+                    case DISK:
+                        int diskMib = spec.diskGib * 1024; // convert from GB to MB
+                        UsageStats.DiskUsage newDisk = new UsageStats.DiskUsage();
+                        newDisk.diskUsed = (long) (diskMib * getLastDouble(graph.values) / 100); // calculate percentage
+                        newDisk.diskTotal = diskMib;
+                        stats.disk = newDisk;
+                        break;
+                }
+                Instant instant = getLastInstant(graph.timestamps);
+                if (instant.isBefore(stats.lastRefreshedAt)) {
+                    stats.lastRefreshedAt = instant;
+                }
+            } catch (NullPointerException e) {
+                // Panopta gave a null value, do nothing
+            }
+        }
+        stats.status = UsageStats.UsageStatsStatus.UPDATED;
+        stats.utilizationId = -1;
+        return stats;
+    }
+
+    private Double getLastDouble(List<Double> list) {
+        Double d = list.get(list.size() - 1);
+        // Panopta will sometimes return null for the last value. In that case, check the second to last value.
+        if (d == null) {
+            d = list.get(list.size() - 2);
+        }
+        return d;
+    }
+
+    private Instant getLastInstant(List<Instant> list) {
+        return list.get(list.size() - 1);
+    }
+
+    private UsageStats getHfsUsage(long hfsVmId) {
+        ServerUsageStats serverUsageStats = serverUsageStatsService.getServerUsage(hfsVmId);
+        if (serverUsageStats == null) {
+            throw new Vps4Exception("USAGE_STATS_UNAVAILABLE", "Usage stats are unavailable at the moment.");
+        }
+        return mapHfsUsageStats(serverUsageStats);
+    }
+
+    private UsageStats mapHfsUsageStats(ServerUsageStats serverUsageStats) {
         UsageStats stats = new UsageStats();
         stats.lastRefreshedAt = serverUsageStats.getRequested().toInstant();
         stats.status = serverUsageStats.getCollected() == null ?
--- web/src/test/java/com/godaddy/vps4/web/vm/ServerUsageStatsResourceTest.java
+++ web/src/test/java/com/godaddy/vps4/web/vm/ServerUsageStatsResourceTest.java
@@ -1,8 +1,12 @@
 package com.godaddy.vps4.web.vm;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyLong;
+import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
@@ -10,7 +14,11 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import java.time.Instant;
 import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.UUID;
 
 import org.junit.After;
@@ -23,11 +31,18 @@ import com.godaddy.hfs.vm.ServerUsageStats;
 import com.godaddy.hfs.vm.Vm;
 import com.godaddy.hfs.vm.VmService;
 import com.godaddy.vps4.jdbc.DatabaseModule;
+import com.godaddy.vps4.panopta.PanoptaDataService;
+import com.godaddy.vps4.panopta.PanoptaDetail;
+import com.godaddy.vps4.panopta.PanoptaGraph;
+import com.godaddy.vps4.panopta.PanoptaService;
+import com.godaddy.vps4.panopta.PanoptaServiceException;
+import com.godaddy.vps4.panopta.PanoptaUsageGraph;
 import com.godaddy.vps4.phase2.CancelActionModule;
 import com.godaddy.vps4.phase2.Phase2ExternalsModule;
 import com.godaddy.vps4.scheduler.api.web.SchedulerWebService;
 import com.godaddy.vps4.security.SecurityModule;
 import com.godaddy.vps4.snapshot.SnapshotModule;
+import com.godaddy.vps4.vm.ServerSpec;
 import com.godaddy.vps4.vm.ServerUsageStatsService;
 import com.godaddy.vps4.vm.VirtualMachine;
 import com.godaddy.vps4.vm.VmModule;
@@ -42,8 +57,15 @@ public class ServerUsageStatsResourceTest {
 
     private VmResource vmResource = mock(VmResource.class);
     private ServerUsageStatsService serverUsageStatsService = mock(ServerUsageStatsService.class);
+    private PanoptaService panoptaService = mock(PanoptaService.class);
+    private PanoptaDataService panoptaDataService = mock(PanoptaDataService.class);
     private ServerUsageStatsResource serverUsageStatsResource, spyResource;
     private VirtualMachine vm;
+    private PanoptaDetail panoptaDetail;
+    private List<PanoptaGraph> usageGraphs;
+    private double cpu;
+    private double mem;
+    private double disk;
 
     @Inject
     private VmService vmService;
@@ -61,6 +83,8 @@ public class ServerUsageStatsResourceTest {
                 protected void configure() {
                     bind(ServerUsageStatsService.class).toInstance(serverUsageStatsService);
                     bind(VmResource.class).toInstance(vmResource);
+                    bind(PanoptaService.class).toInstance(panoptaService);
+                    bind(PanoptaDataService.class).toInstance(panoptaDataService);
                     SchedulerWebService swServ = mock(SchedulerWebService.class);
                     bind(SchedulerWebService.class).toInstance(swServ);
                 }
@@ -73,6 +97,30 @@ public class ServerUsageStatsResourceTest {
         serverUsageStatsResource = injector.getInstance(ServerUsageStatsResource.class);
         spyResource = spy(serverUsageStatsResource);
         vm = createTestVm();
+        panoptaDetail = new PanoptaDetail(1, vm.vmId, "partnerCustomerKey",
+                                          "customerKey", 42, "serverKey",
+                                          Instant.now(), Instant.MAX);
+        cpu = Math.random() * 100;
+        mem = Math.random() * 100;
+        disk = Math.random() * 100;
+        setUpUsageGraphs();
+    }
+
+    private void setUpUsageGraphs() {
+        usageGraphs = new ArrayList<>();
+        usageGraphs.add(setUpUsageGraph(PanoptaGraph.Type.CPU, cpu));
+        usageGraphs.add(setUpUsageGraph(PanoptaGraph.Type.RAM, mem));
+        usageGraphs.add(setUpUsageGraph(PanoptaGraph.Type.DISK, disk));
+    }
+
+    private PanoptaGraph setUpUsageGraph(PanoptaGraph.Type type, Double value) {
+        PanoptaGraph g = new PanoptaUsageGraph();
+        g.type = type;
+        g.timestamps = new ArrayList<>();
+        g.timestamps.add(Instant.now());
+        g.values = new ArrayList<>();
+        g.values.add(value);
+        return g;
     }
 
     private ServerUsageStats createDummyServerUsageStats() {
@@ -91,15 +139,103 @@ public class ServerUsageStatsResourceTest {
         vm = new VirtualMachine();
         vm.hfsVmId = 1234;
         vm.vmId = UUID.randomUUID();
+        vm.spec = new ServerSpec();
+        vm.spec.memoryMib = 4096;
+        vm.spec.diskGib = 40;
         return vm;
     }
 
-    @After
-    public void tearDown() throws Exception {
+    @Test
+    public void getPanoptaUsage() throws PanoptaServiceException {
+        doNothing().when(spyResource).verifyServerIsActive(anyLong());
+        when(vmResource.getVm(vm.vmId)).thenReturn(vm);
+        when(panoptaDataService.getPanoptaDetails(vm.vmId)).thenReturn(panoptaDetail);
+        when(panoptaService.getUsageGraphs(vm.vmId, "hour")).thenReturn(usageGraphs);
+
+        UsageStats stats = spyResource.getUsage(vm.vmId);
+
+        verify(vmResource, times(1)).getVm(vm.vmId);
+        verify(panoptaService, times(1)).getUsageGraphs(vm.vmId, "hour");
+        assertNotNull("Stats cannot be null.", stats);
+        assertEquals(cpu, stats.cpu.cpuUsagePercent, 0.01);
+        assertEquals((long) (vm.spec.memoryMib * mem / 100), stats.mem.memUsed);
+        assertEquals(vm.spec.memoryMib, stats.mem.memTotal);
+        assertEquals((long) (vm.spec.diskGib * 1024 * disk / 100), stats.disk.diskUsed);
+        assertEquals(vm.spec.diskGib * 1024, stats.disk.diskTotal);
     }
 
     @Test
-    public void getsServerUsage() {
+    public void getPanoptaUsageWithNullLastValue() throws PanoptaServiceException {
+        for (PanoptaGraph graph : usageGraphs) {
+            graph.timestamps.add(Instant.now());
+            graph.values.add(null);
+        }
+
+        doNothing().when(spyResource).verifyServerIsActive(anyLong());
+        when(vmResource.getVm(vm.vmId)).thenReturn(vm);
+        when(panoptaDataService.getPanoptaDetails(vm.vmId)).thenReturn(panoptaDetail);
+        when(panoptaService.getUsageGraphs(vm.vmId, "hour")).thenReturn(usageGraphs);
+
+        UsageStats stats = spyResource.getUsage(vm.vmId);
+
+        verify(vmResource, times(1)).getVm(vm.vmId);
+        verify(panoptaService, times(1)).getUsageGraphs(vm.vmId, "hour");
+        assertNotNull("Stats cannot be null.", stats);
+        assertEquals(cpu, stats.cpu.cpuUsagePercent, 0.01);
+        assertEquals((long) (vm.spec.memoryMib * mem / 100), stats.mem.memUsed);
+        assertEquals(vm.spec.memoryMib, stats.mem.memTotal);
+        assertEquals((long) (vm.spec.diskGib * 1024 * disk / 100), stats.disk.diskUsed);
+        assertEquals(vm.spec.diskGib * 1024, stats.disk.diskTotal);
+    }
+
+    @Test
+    public void getPanoptaUsageWithMultipleNullValues() throws PanoptaServiceException {
+        for (PanoptaGraph graph : usageGraphs) {
+            graph.timestamps.add(Instant.now());
+            graph.timestamps.add(Instant.now());
+            graph.values.add(null);
+            graph.values.add(null);
+        }
+
+        doNothing().when(spyResource).verifyServerIsActive(anyLong());
+        when(vmResource.getVm(vm.vmId)).thenReturn(vm);
+        when(panoptaDataService.getPanoptaDetails(vm.vmId)).thenReturn(panoptaDetail);
+        when(panoptaService.getUsageGraphs(vm.vmId, "hour")).thenReturn(usageGraphs);
+
+        UsageStats stats = spyResource.getUsage(vm.vmId);
+
+        verify(vmResource, times(1)).getVm(vm.vmId);
+        verify(panoptaService, times(1)).getUsageGraphs(vm.vmId, "hour");
+        assertNotNull("Stats cannot be null.", stats);
+        assertNull(stats.cpu);
+        assertNull(stats.mem);
+        assertNull(stats.disk);
+    }
+
+    @Test(expected = Vps4Exception.class)
+    public void getPanoptaUsageInActiveVm() throws PanoptaServiceException {
+        Vm inactiveVm = new Vm();
+        inactiveVm.status = "STOPPED";
+
+        when(vmResource.getVm(any(UUID.class))).thenReturn(vm);
+        when(panoptaDataService.getPanoptaDetails(any(UUID.class))).thenReturn(panoptaDetail);
+        when(panoptaService.getUsageGraphs(vm.vmId, "hour")).thenReturn(usageGraphs);
+        when(vmService.getVm(vm.hfsVmId)).thenReturn(inactiveVm);
+
+        spyResource.getUsage(UUID.randomUUID());
+    }
+
+    @Test(expected = Vps4Exception.class)
+    public void getPanoptaUsageNullStats() throws PanoptaServiceException {
+        when(vmResource.getVm(any(UUID.class))).thenReturn(vm);
+        when(panoptaDataService.getPanoptaDetails(any(UUID.class))).thenReturn(panoptaDetail);
+        when(panoptaService.getUsageGraphs(any(UUID.class), anyString())).thenReturn(null);
+
+        spyResource.getUsage(UUID.randomUUID());
+    }
+
+    @Test
+    public void getHfsUsage() throws PanoptaServiceException {
         ServerUsageStats serverUsageStats = createDummyServerUsageStats();
         doNothing().when(spyResource).verifyServerIsActive(anyLong());
         when(vmResource.getVm(any(UUID.class))).thenReturn(vm);
@@ -109,11 +245,11 @@ public class ServerUsageStatsResourceTest {
 
         verify(vmResource, times(1)).getVm(any(UUID.class));
         verify(serverUsageStatsService, times(1)).getServerUsage(anyLong());
-        assertTrue("Stats cannot be null.", stats!= null);
+        assertTrue("Stats cannot be null.", stats != null);
     }
 
     @Test(expected = Vps4Exception.class)
-    public void getsServerUsageInActiveVm() {
+    public void getHfsUsageInActiveVm() throws PanoptaServiceException {
         Vm inactiveVm = new Vm();
         inactiveVm.status = "STOPPED";
 
@@ -126,11 +262,10 @@ public class ServerUsageStatsResourceTest {
     }
 
     @Test(expected = Vps4Exception.class)
-    public void getsServerUsageNullStats() {
+    public void getHfsUsageNullStats() throws PanoptaServiceException {
         when(vmResource.getVm(any(UUID.class))).thenReturn(vm);
         when(serverUsageStatsService.getServerUsage(anyLong())).thenReturn(null);
 
         spyResource.getUsage(UUID.randomUUID());
     }
-
 }
