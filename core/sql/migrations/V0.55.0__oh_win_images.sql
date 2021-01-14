e18a015cc25148f003a52feff60fd03adaedf88b
Scott Johnson <sjohnson@godaddy.com>
2021-01-14 11:21:00 -0700

    DB migration for OH windows images (#988)

---
+++ core/sql/migrations/V0.55.0__oh_win_images.sql
@@ -0,0 +1,7 @@
+-- Add windows images for Optimized Hosting.
+INSERT INTO image
+  (hfs_name, control_panel_id, os_type_id, name, server_type_id)
+VALUES
+  ('hfs-win2019', 0, 2, 'Windows 2019', 3),
+  ('hfs-win2019-plesk', 2, 2, 'Windows 2019 (Plesk)', 3);
+
