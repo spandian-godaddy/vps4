5abe10da2b472f9b647f0956cf6dca702c884bef
David <dorr@godaddy.com>
2022-02-03 10:23:06 -0700

    Fix the backfill to account for shopper migrations (#69)

--- core/sql/migrations/V0.71.0__add_vps4_vm_user_id_to_project.sql
+++ core/sql/migrations/V0.71.0__add_vps4_vm_user_id_to_project.sql
@@ -1,6 +1,10 @@
 ALTER TABLE project ADD COLUMN vps4_user_id bigint references vps4_user(vps4_user_id);
 
 
+update project set vps4_user_id = (select vps4_user_id from user_project_privilege where project.project_id = user_project_privilege.project_id and user_project_privilege.valid_until = 'infinity');
+update project set vps4_user_id = (select vps4_user_id from user_project_privilege where project.project_id = user_project_privilege.project_id order by user_project_privilege.valid_on desc limit 1) where vps4_user_id is null;
+
+
 create or replace function create_project(p_project_name character varying, p_owner_user_id bigint, p_sgid_prefix character varying) returns bigint
     language plpgsql
 as
@@ -17,6 +21,3 @@ BEGIN
 
 END;
 $$;
-
-
-update project set vps4_user_id = (select vps4_user_id from user_project_privilege where project.project_id = user_project_privilege.project_id);
