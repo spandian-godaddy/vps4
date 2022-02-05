f052ff458d5212149200f9a65b0dd32c126b23ae
Scott Mangiapane <85906241+smangiapane-godaddy@users.noreply.github.com>
2022-02-04 17:34:50 -0700

    Add new windows 2022  images (#73)

---
+++ core/sql/migrations/V0.71.1__add_windows_2022_image.sql
@@ -0,0 +1,5 @@
+INSERT INTO image
+    (hfs_name, control_panel_id, os_type_id, name, server_type_id)
+VALUES
+    ('hfs-win2022', 0, 2, 'Windows 2022', 3),
+    ('hfs-win2022-plesk', 2, 2, 'Windows 2022 (Plesk)', 3);
