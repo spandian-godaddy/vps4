462cfe8aca8eab3918305c7039f275de2e8115be
Scott Mangiapane <85906241+smangiapane-godaddy@users.noreply.github.com>
2023-10-11 09:55:32 -0500

    Removed special cert-JWT over mutual auth client (#470)

--- core/src/main/java/com/godaddy/vps4/hfs/HfsClientModule.java
+++ core/src/main/java/com/godaddy/vps4/hfs/HfsClientModule.java
@@ -1,6 +1,6 @@
 package com.godaddy.vps4.hfs;
 
-import static com.godaddy.vps4.client.ClientUtils.getHfsClientProvider;
+import static com.godaddy.vps4.client.ClientUtils.getCertJwtAuthServiceProvider;
 
 import com.godaddy.hfs.cpanel.CPanelService;
 import com.godaddy.hfs.dns.HfsDnsService;
@@ -20,31 +20,31 @@ public class HfsClientModule extends AbstractModule {
     @Override
     public void configure() {
         bind(VmService.class)
-                .toProvider(getHfsClientProvider(VmService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(VmService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(CPanelService.class)
-                .toProvider(getHfsClientProvider(CPanelService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(CPanelService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(PleskService.class)
-                .toProvider(getHfsClientProvider(PleskService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(PleskService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(SysAdminService.class)
-                .toProvider(getHfsClientProvider(SysAdminService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(SysAdminService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(MailRelayService.class)
-                .toProvider(getHfsClientProvider(MailRelayService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(MailRelayService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(ECommService.class)
-                .toProvider(getHfsClientProvider(ECommService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(ECommService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(NetworkServiceV2.class)
-                .toProvider(getHfsClientProvider(NetworkServiceV2.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(NetworkServiceV2.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(HfsDnsService.class)
-                .toProvider(getHfsClientProvider(HfsDnsService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(HfsDnsService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
         bind(SnapshotService.class)
-                .toProvider(getHfsClientProvider(SnapshotService.class, "hfs.base.url", CertJwtApi.HFS))
+                .toProvider(getCertJwtAuthServiceProvider(SnapshotService.class, "hfs.base.url", CertJwtApi.HFS))
                 .in(Singleton.class);
     }
 }
