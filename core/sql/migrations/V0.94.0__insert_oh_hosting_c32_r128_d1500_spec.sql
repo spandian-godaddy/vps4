70d0e2bc996e19dc4be2793c495dd077b00181dd
David <dorr@godaddy.com>
2023-10-20 13:28:16 -0700

    DB migration to add the oh.hosting.c32.r128.d1500 server spec (#473)
    * Add migration for oh.hosting.c32.r128.d1500
    
    * Change from 1 to 4 IPs

---
+++ core/sql/migrations/V0.94.0__insert_oh_hosting_c32_r128_d1500_spec.sql
@@ -0,0 +1,4 @@
+INSERT INTO virtual_machine_spec
+(tier, spec_name, cpu_core_count, memory_mib, disk_gib, name, ip_address_count, server_type_id)
+VALUES
+(48, 'oh.hosting.c32.r128.d1500', 32, 131072, 1500, '32.1', 4, 3)
