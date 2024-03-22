0c244130dd1f31030750d9030b11de3d1cf2ffcb
David <dorr@godaddy.com>
2024-03-22 16:28:55 -0700

    Remove the ProdMeta resource and services (#586)
    * Add the credit table, drop the prod_meta
    
    * Create the credit table, and Vps4LocalCreditService and JDBC
    
    * Add the JdbcVps4LocalCreditService and unit tests.
    
    * Add unit test for getCreditByVmId
    
    * Add resource
    
    * Remove the prod meta service tests for now.
    
    * Add Get Credits by shopperId and customerId
    
    * Remove public modifiers on the Vps4LocalCreditService.java
    
    * Add foreign key for data center id
    
    * Remove the reference key to the datacenter
    
    * Remove ProdMeta services and resources

--- core/src/main/java/com/godaddy/vps4/credit/CreditModule.java
+++ core/src/main/java/com/godaddy/vps4/credit/CreditModule.java
@@ -1,8 +1,6 @@
 package com.godaddy.vps4.credit;
 
 import com.godaddy.vps4.credit.jdbc.JdbcVps4LocalCreditService;
-import com.godaddy.vps4.prodMeta.ProdMetaService;
-import com.godaddy.vps4.prodMeta.jdbc.JdbcProdMetaService;
 import com.google.inject.AbstractModule;
 
 public class CreditModule extends AbstractModule{
@@ -10,7 +8,6 @@ public class CreditModule extends AbstractModule{
     @Override
     public void configure() {
         bind(CreditService.class).to(ECommCreditService.class);
-        bind(ProdMetaService.class).to(JdbcProdMetaService.class);
         bind(Vps4LocalCreditService.class).to(JdbcVps4LocalCreditService.class);
     }
 }
